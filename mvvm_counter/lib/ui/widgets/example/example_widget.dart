aafa35aa89fda556bc388f9a3236c2339e9f43fa
Aleksey Kuzmin <kboomska@gmail.com>
2023-05-18 16:10:01 +0300

    Lesson 80: Architecture. MVVM Counter (Created simple counter)

--- mvvm_counter/lib/ui/widgets/example/example_widget.dart
+++ mvvm_counter/lib/ui/widgets/example/example_widget.dart
@@ -1,13 +1,49 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-class ExampleWidget extends StatelessWidget {
+class ExampleWidget extends StatefulWidget {
   const ExampleWidget({super.key});
+
+  @override
+  State<ExampleWidget> createState() => _ExampleWidgetState();
+}
+
+class _ExampleWidgetState extends State<ExampleWidget> {
+  var _value = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(),
+        child: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  _value = max(_value - 1, 0);
+                  setState(() {});
+                },
+                child: const Text('-'),
+              ),
+              SizedBox(
+                width: 60,
+                child: Text(
+                  '$_value',
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  _value += 1;
+                  setState(() {});
+                },
+                child: const Text('+'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
