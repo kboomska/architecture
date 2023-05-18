8f7ca775eee5de70166edeaa3b59b2cea5988ba8
Aleksey Kuzmin <kboomska@gmail.com>
2023-05-18 16:19:30 +0300

    Lesson 80: Architecture. MVVM Counter (Added SharedPreferences)

--- mvvm_counter/lib/ui/widgets/example/example_widget.dart
+++ mvvm_counter/lib/ui/widgets/example/example_widget.dart
@@ -2,6 +2,8 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 
+import 'package:shared_preferences/shared_preferences.dart';
+
 class ExampleWidget extends StatefulWidget {
   const ExampleWidget({super.key});
 
@@ -12,6 +14,18 @@ class ExampleWidget extends StatefulWidget {
 class _ExampleWidgetState extends State<ExampleWidget> {
   var _value = 0;
 
+  void loadValue() async {
+    final sharedPreferences = await SharedPreferences.getInstance();
+    _value = sharedPreferences.getInt('age') ?? 0;
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadValue();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,8 +35,11 @@ class _ExampleWidgetState extends State<ExampleWidget> {
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               ElevatedButton(
-                onPressed: () {
+                onPressed: () async {
                   _value = max(_value - 1, 0);
+                  final sharedPreferences =
+                      await SharedPreferences.getInstance();
+                  sharedPreferences.setInt('age', _value);
                   setState(() {});
                 },
                 child: const Text('-'),
@@ -35,8 +52,11 @@ class _ExampleWidgetState extends State<ExampleWidget> {
                 ),
               ),
               ElevatedButton(
-                onPressed: () {
+                onPressed: () async {
                   _value += 1;
+                  final sharedPreferences =
+                      await SharedPreferences.getInstance();
+                  sharedPreferences.setInt('age', _value);
                   setState(() {});
                 },
                 child: const Text('+'),
