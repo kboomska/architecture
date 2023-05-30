12b71dcac621e9d4d10ca7c98bfa9f1f6450d292
Aleksey Kuzmin <kboomska@gmail.com>
2023-05-30 11:25:13 +0300

    Lesson 95: Architecture. BLoC Counter. BLoC (BlocObserver)

---
+++ bloc_counter/lib/domain/blocs/blocs_observer.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class BlocsObserver extends BlocObserver {
+  @override
+  void onCreate(BlocBase bloc) {
+    super.onCreate(bloc);
+    print(bloc);
+  }
+
+  @override
+  void onEvent(Bloc bloc, Object? event) {
+    super.onEvent(bloc, event);
+    print(bloc);
+    print(event);
+  }
+
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    super.onError(bloc, error, stackTrace);
+    print(error);
+  }
+}
--- bloc_counter/lib/main.dart
+++ bloc_counter/lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:bloc_counter/domain/blocs/blocs_observer.dart';
 import 'package:bloc_counter/ui/widgets/app/my_app.dart';
 
 void main() {
   const app = MyApp();
+  Bloc.observer = BlocsObserver();
   runApp(app);
 }
