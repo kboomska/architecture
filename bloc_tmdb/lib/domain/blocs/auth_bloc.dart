ff6a07cbada73496323f0cb7f3c6f436a847a9ef
Aleksey Kuzmin <kboomska@gmail.com>
2023-05-31 11:10:05 +0300

    Lesson 97: Architecture. BLoC TMDB (auth bloc refactoring)

---
+++ bloc_tmdb/lib/domain/blocs/auth_bloc.dart
@@ -0,0 +1,129 @@
+import 'package:bloc_concurrency/bloc_concurrency.dart';
+import 'package:bloc/bloc.dart';
+
+import 'package:bloc_tmdb/domain/data_providers/session_data_provider.dart';
+import 'package:bloc_tmdb/domain/api_client/account_api_client.dart';
+import 'package:bloc_tmdb/domain/api_client/auth_api_client.dart';
+
+abstract class AuthEvent {}
+
+class AuthCheckStatusEvent extends AuthEvent {}
+
+class AuthLogoutEvent extends AuthEvent {}
+
+class AuthLoginEvent extends AuthEvent {
+  final String login;
+  final String password;
+
+  AuthLoginEvent({
+    required this.login,
+    required this.password,
+  });
+}
+
+enum AuthStateStatus { authorized, notAuthorized, inProgress }
+
+abstract class AuthState {}
+
+class AuthNotAuthorizedState extends AuthState {
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is AuthNotAuthorizedState && runtimeType == other.runtimeType;
+
+  @override
+  int get hashCode => 0;
+}
+
+class AuthAuthorizedState extends AuthState {
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is AuthAuthorizedState && runtimeType == other.runtimeType;
+
+  @override
+  int get hashCode => 0;
+}
+
+class AuthFailureState extends AuthState {
+  final Object error;
+
+  AuthFailureState(this.error);
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is AuthFailureState &&
+          runtimeType == other.runtimeType &&
+          other.error == error;
+
+  @override
+  int get hashCode => error.hashCode;
+}
+
+class AuthInProgressState extends AuthState {}
+
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  final _sessionDataProvider = SessionDataProvider();
+  final _accountApiClient = AccountApiClient();
+  final _authApiClient = AuthApiClient();
+
+  AuthBloc() : super(AuthNotAuthorizedState()) {
+    on<AuthEvent>(
+      (event, emit) async {
+        if (event is AuthCheckStatusEvent) {
+          onAuthCheckStatusEvent(event, emit);
+        } else if (event is AuthLoginEvent) {
+          onAuthLoginEvent(event, emit);
+        } else if (event is AuthLogoutEvent) {
+          onAuthLogoutEvent(event, emit);
+        }
+      },
+      transformer: sequential(),
+    );
+
+    add(AuthCheckStatusEvent());
+  }
+
+  void onAuthCheckStatusEvent(
+    AuthCheckStatusEvent event,
+    Emitter<AuthState> emit,
+  ) async {
+    final sessionId = await _sessionDataProvider.getSessionId();
+    final newState =
+        sessionId != null ? AuthAuthorizedState() : AuthNotAuthorizedState();
+    emit(newState);
+  }
+
+  void onAuthLoginEvent(
+    AuthLoginEvent event,
+    Emitter<AuthState> emit,
+  ) async {
+    try {
+      final sessionId = await _authApiClient.auth(
+        username: event.login,
+        password: event.password,
+      );
+      final accountId = await _accountApiClient.getAccountInfo(sessionId);
+
+      await _sessionDataProvider.setSessionId(sessionId);
+      await _sessionDataProvider.setAccountId(accountId);
+      emit(AuthAuthorizedState());
+    } catch (e) {
+      emit(AuthFailureState(e));
+    }
+  }
+
+  void onAuthLogoutEvent(
+    AuthLogoutEvent event,
+    Emitter<AuthState> emit,
+  ) async {
+    try {
+      await _sessionDataProvider.removeSessionId();
+      await _sessionDataProvider.removeAccountId();
+      emit(AuthNotAuthorizedState());
+    } catch (e) {
+      emit(AuthFailureState(e));
+    }
+  }
+}
