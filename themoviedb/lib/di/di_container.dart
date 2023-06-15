bf2bf92821eaa3cdae2a02500ee5be2383f1767e
Aleksey Kuzmin <kboomska@gmail.com>
2023-06-15 10:26:33 +0300

    Lesson 106: Architecture. DI MVVM TMDB (refactoring screen factory)

--- themoviedb/lib/di/di_container.dart
+++ themoviedb/lib/di/di_container.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
 
+import 'package:provider/provider.dart';
+
+import 'package:themoviedb/ui/widgets/movie_details/movie_details_widget_model.dart';
+import 'package:themoviedb/ui/widgets/movie_trailer/movie_trailer_widget.dart';
+import 'package:themoviedb/ui/widgets/movie_details/movie_details_widget.dart';
+import 'package:themoviedb/ui/widgets/tv_show_list/tv_show_list_widget.dart';
+import 'package:themoviedb/ui/widgets/movie_list/movie_list_view_model.dart';
+import 'package:themoviedb/ui/widgets/main_screen/main_screen_widget.dart';
+import 'package:themoviedb/ui/widgets/movie_list/movie_list_widget.dart';
+import 'package:themoviedb/ui/widgets/loader/loader_view_model.dart';
+import 'package:themoviedb/ui/widgets/auth/auth_view_model.dart';
+import 'package:themoviedb/ui/widgets/loader/loader_widget.dart';
 import 'package:themoviedb/ui/navigation/main_navigation.dart';
+import 'package:themoviedb/ui/widgets/auth/auth_widget.dart';
+import 'package:themoviedb/ui/widgets/news/news_widget.dart';
 import 'package:themoviedb/ui/widgets/app/my_app.dart';
 import 'package:themoviedb/main.dart';
 
@@ -13,12 +27,70 @@ class _AppFactoryDefault implements AppFactory {
 
   @override
   Widget makeApp() => MyApp(
-        navigation: _diContainer._mainNavigation,
+        navigation: _diContainer.makeMyAppNavigation(),
       );
 }
 
 class _DIContainer {
-  final MyAppNavigation _mainNavigation = const MainNavigation();
-
   const _DIContainer();
+
+  ScreenFactory makeScreenFactory() => const ScreenFactoryDefault();
+  MyAppNavigation makeMyAppNavigation() => MainNavigation(makeScreenFactory());
+}
+
+class ScreenFactoryDefault implements ScreenFactory {
+  const ScreenFactoryDefault();
+
+  @override
+  Widget makeLoader() {
+    return Provider(
+      lazy: false,
+      create: (context) => LoaderViewModel(context),
+      child: const LoaderWidget(),
+    );
+  }
+
+  @override
+  Widget makeAuth() {
+    return ChangeNotifierProvider(
+      create: (_) => AuthViewModel(),
+      child: const AuthWidget(),
+    );
+  }
+
+  @override
+  Widget makeMainScreen() {
+    return const MainScreenWidget();
+  }
+
+  @override
+  Widget makeMovieDetails(int movieId) {
+    return ChangeNotifierProvider(
+      create: (_) => MovieDetailsWidgetModel(movieId),
+      child: const MovieDetailsWidget(),
+    );
+  }
+
+  @override
+  Widget makeMovieTrailer(String youtubeKey) {
+    return MovieTrailerWidget(youtubeKey: youtubeKey);
+  }
+
+  @override
+  Widget makeNewsList() {
+    return const NewsWidget();
+  }
+
+  @override
+  Widget makeMovieList() {
+    return ChangeNotifierProvider(
+      create: (_) => MovieListViewModel(),
+      child: const MovieListWidget(),
+    );
+  }
+
+  @override
+  Widget makeTVShowList() {
+    return const TVShowListWidget();
+  }
 }
--- themoviedb/lib/ui/navigation/main_navigation.dart
+++ themoviedb/lib/ui/navigation/main_navigation.dart
@@ -1,20 +1,30 @@
 import 'package:flutter/material.dart';
 
 import 'package:themoviedb/ui/navigation/main_navigation_route_names.dart';
-import 'package:themoviedb/domain/factories/screen_factory.dart';
 import 'package:themoviedb/ui/widgets/app/my_app.dart';
 
-class MainNavigation implements MyAppNavigation {
-  final _screenFactory = const ScreenFactory();
+abstract class ScreenFactory {
+  Widget makeLoader();
+  Widget makeAuth();
+  Widget makeMainScreen();
+  Widget makeMovieDetails(int movieId);
+  Widget makeMovieTrailer(String youtubeKey);
+  Widget makeNewsList();
+  Widget makeMovieList();
+  Widget makeTVShowList();
+}
 
-  const MainNavigation();
+class MainNavigation implements MyAppNavigation {
+  final ScreenFactory screenFactory;
+
+  const MainNavigation(this.screenFactory);
 
   @override
   Map<String, Widget Function(BuildContext)> get routes => {
-        MainNavigationRouteNames.loader: (_) => _screenFactory.makeLoader(),
-        MainNavigationRouteNames.auth: (_) => _screenFactory.makeAuth(),
+        MainNavigationRouteNames.loader: (_) => screenFactory.makeLoader(),
+        MainNavigationRouteNames.auth: (_) => screenFactory.makeAuth(),
         MainNavigationRouteNames.mainScreen: (_) =>
-            _screenFactory.makeMainScreen(),
+            screenFactory.makeMainScreen(),
       };
 
   @override
@@ -24,13 +34,13 @@ class MainNavigation implements MyAppNavigation {
         final arguments = settings.arguments;
         final movieId = arguments is int ? arguments : 0;
         return MaterialPageRoute(
-          builder: (_) => _screenFactory.makeMovieDetails(movieId),
+          builder: (_) => screenFactory.makeMovieDetails(movieId),
         );
       case MainNavigationRouteNames.movieTrailer:
         final arguments = settings.arguments;
         final youtubeKey = arguments is String ? arguments : '';
         return MaterialPageRoute(
-          builder: (_) => _screenFactory.makeMovieTrailer(youtubeKey),
+          builder: (_) => screenFactory.makeMovieTrailer(youtubeKey),
         );
       default:
         const widget = Text('Navigation error!!!');
